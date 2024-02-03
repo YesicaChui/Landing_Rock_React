291a40ff7eeb805afd91f419712915a96e410682
YesicaChui <yesicachuic@gmail.com>
2024-02-03 16:05:17 -0500

    estilizando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex justify-around p-2 bg-[#343a40]'>
+      <p className='text-white text-lg font-semibold'>Start Bootstrap</p>
+      <div className='flex gap-5 text-white'>
+        <p>Home</p>
+        <p>About</p>
+        <p>Services</p>
+        <p>Contact</p>
+      </div>
+    </div>
   )
 }
 
