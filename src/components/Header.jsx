4a2966253c0d24e6c2d66002aa93b815faf92e88
YesicaChui <yesicachuic@gmail.com>
2024-02-03 17:22:54 -0500

    agregando cursor a header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ const Header = () => {
   return (
     <div className='flex justify-around p-2 bg-[#343a40]'>
       <p className='text-white text-lg font-semibold'>Start Bootstrap</p>
-      <div className='flex gap-5 text-white'>
-        <p>Home</p>
-        <p>About</p>
-        <p>Services</p>
-        <p>Contact</p>
+      <div className='flex gap-5 text-white '>
+        <p className='cursor-pointer hover:font-bold hover:text-lg'>Home</p>
+        <p className='cursor-pointer hover:font-bold hover:text-lg'>About</p>
+        <p className='cursor-pointer hover:font-bold hover:text-lg'>Services</p>
+        <p className='cursor-pointer hover:font-bold hover:text-lg'>Contact</p>
       </div>
     </div>
   )
